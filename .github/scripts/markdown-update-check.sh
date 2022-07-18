2d3a5a2ccdd2d5b90a0363165152d4d3de463df4
Alexandr Devyatkin <adevyatkin@web3tech.ru>
2022-07-18 18:27:59 +0300

    check

--- .github/scripts/markdown-update-check.sh
+++ .github/scripts/markdown-update-check.sh
@@ -1,6 +1,6 @@
 STATUSCODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -X 'GET' 'https://api-dev.waves.tech/api/v1/test' \
   -H 'accept: */*' \
-  -H 'secret: ${{ secrets.SECRET_FOR_UPDATE }}')
+  -H "secret: $1")
 if test "$STATUSCODE" -ne 200; then
   exit 1
 fi
