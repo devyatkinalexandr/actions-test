3d4c47e0b7c3e97f9690f126fcb7802809aa7b1b
Alexandr Devyatkin <adevyatkin@web3tech.ru>
2022-07-18 18:06:33 +0300

    check

--- .github/scripts/markdown-update-check.sh
+++ .github/scripts/markdown-update-check.sh
@@ -2,5 +2,5 @@ STATUSCODE=$(curl -LI -o /dev/null -w '%{http_code}\n' -X 'GET' 'https://api-dev
   -H 'accept: */*' \
   -H 'secret: ${{ secrets.SECRET_FOR_UPDATE }}')
 if test "$STATUSCODE" -ne 200; then
-  exix 1
+  exit 1
 fi
